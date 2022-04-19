5c4d37cc64400a1e2e5ac8140a1c76f358745c5e
jgrossrieder <julien.grossrieder@ch.meggitt.com>
2022-04-19 13:28:35 +0200

    Add project files.

---
+++ Src/Central.Application/SomeClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Central.Application
+{
+    public class SomeClass
+    {
+        public int Double(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Tests/Central.Application.Test/SomeClassTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Central.Application.Test
+{
+    [TestFixture]
+    public class SomeClassTest
+    {
+        [Test]
+        public void TestSomething()
+        {
+            SomeClass someClass = new SomeClass();
+            int result = someClass.Double(5, 2);
+
+            Assert.AreEqual(10, result);
+        }
+    }
+}
